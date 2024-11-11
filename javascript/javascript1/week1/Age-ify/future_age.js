641743b21bb3900e525c8c9af31071fcf3f984a2
gayathri <gayathri.cake@gmail.com>
2024-11-11 18:05:21 +0100

    Age-ify (A future age calculator) completed

---
+++ javascript/javascript1/week1/Age-ify/future_age.js
@@ -0,0 +1,7 @@
+const yearOfBirth = 1996;
+const yearFuture = 2045;
+console.log("Data type of yearOfBirth is " + typeof yearOfBirth);
+console.log("Data type of yearFuture is " + typeof yearFuture);
+
+const age = yearFuture - yearOfBirth;
+console.log("You will be " + age + " years old in " + yearFuture);
