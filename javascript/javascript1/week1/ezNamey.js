b6d598b418ad7142839154f7d0cc315dd7a237b6
jalilhu <jhus12b@gmail.com>
2024-11-11 19:11:22 +0100

    Startup name generator

---
+++ javascript/javascript1/week1/ezNamey.js
@@ -0,0 +1,8 @@
+const firstWords= ["Spark", "Neo", "Prime", "Wave", "Pulse", "Orbit", "Cloud", "Swift", "Vertex", "Quantum"];
+const secondWords = ["Tech", "Solutions", "Hub", "Labs", "Works", "Systems", "Dynamics", "Forge", "Vision", "Edge"];
+const randomNumberOne = Math.floor(Math.random() * 10);
+const randomNumberTwo = Math.floor(Math.random() * 10);
+
+const startupName = firstWords[randomNumberOne] + secondWords[randomNumberTwo];
+
+console.log(startupName)
