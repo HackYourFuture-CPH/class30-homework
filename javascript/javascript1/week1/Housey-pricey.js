ce808e8ff8d105cfd273aaf5e63d9073ba31c9e2
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2024-11-11 18:45:54 +0100

    add solution using function

--- javascript/javascript1/week1/Housey-pricey.js
+++ javascript/javascript1/week1/Housey-pricey.js
@@ -25,3 +25,34 @@ if(juliaHouse.price <= juliaExpectedPrice )
 else{
     console.log("Julia is paying too much!")
 }
+//I tried to use a function here just for more practice :)
+/*
+function housePrice(volumeInMeters, gardenSizeInM2){
+    return volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300; 
+}
+const peterHouse = {
+    width : 8,
+    height:10,
+    depth:10,
+    garden:100,
+    price:2500000
+}
+const volumeInMetersP = peterHouse.width*peterHouse.height*peterHouse.depth;
+if(housePrice(volumeInMetersP , peterHouse.garden) > peterHouse.price)
+    console.log("Peter is paying too little!");
+else
+    console.log("Peter is paying too much!")
+
+const juliaHouse = {
+    width : 5,
+    height:8,
+    depth:11,
+    garden:70,
+    price:1000000
+}
+const volumeInMetersJ = juliaHouse.width*juliaHouse.height*juliaHouse.depth;
+if(housePrice(volumeInMetersJ , juliaHouse.garden) > juliaHouse.price)
+    console.log("Julia is paying too little!");
+else
+    console.log("Julia is paying too much!")
+*/
