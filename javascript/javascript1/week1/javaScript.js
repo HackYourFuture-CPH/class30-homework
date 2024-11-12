2d0847cd6927413773b95285590e733a16caf4a5
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-11-12 20:48:35 +0100

    added space

--- javascript/javascript1/week1/javaScript.js
+++ javascript/javascript1/week1/javaScript.js
@@ -12,7 +12,7 @@ const dogYearInHumanYear = dogYearFuture - dogYearOfBirth;
 const dogYearInDogYear = dogYearInHumanYear * 7;
 
 if (shouldShowResultInDogYears) {
-    console.log("Your dog will be " + dogYearInDogYear + "dog years old in " + dogYearFuture + ".");
+    console.log("Your dog will be " + dogYearInDogYear + " dog years old in " + dogYearFuture + ".");
 } else {
-    console.log("Your dog will be " + dogYearInHumanYear + "dog years old in " + dogYearFuture + ".");
+    console.log("Your dog will be " + dogYearInHumanYear + " dog years old in " + dogYearFuture + ".");
 }
