5d7d45e1e26a4dc6ebc8eecdcede5a53c0dec2e4
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-11-12 19:58:13 +0100

    Exc 1

---
+++ javascript/javascript1/week1/javaScript.js
@@ -0,0 +1,7 @@
+//Age-ify (A future age calculator)
+const yearOfBirth= 1993;
+const yearFuture= 2050;
+const age= yearFuture - yearOfBirth;
+console.log("You will be " + age +" years old in " + yearFuture + ".");
+
+//Goodboy-Oldboy (A dog age calculator)
