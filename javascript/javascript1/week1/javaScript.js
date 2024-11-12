f6f882c13dea0e2149ed3f232bff4f7d8238480c
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-11-12 21:32:30 +0100

    julia

--- javascript/javascript1/week1/javaScript.js
+++ javascript/javascript1/week1/javaScript.js
@@ -21,21 +21,21 @@ if (shouldShowResultInDogYears) {
 let wide;
 let deep;
 let high;
-let volumeInMeters= wide* deep* high;
 let gardenSizeInM2;
 
 //Peter
 wide= 8;
 deep= 10;
 high= 10;
+const volumeInMeters= wide* deep* high;
 gardenSizeInM2= 100;
 const peterHousePrice = 2500000;
 const peterCalculatedHousePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 
 if (peterHousePrice <= peterCalculatedHousePrice ) {
-    console.log("The price is okay.");
+    console.log("Peter's house: The price is okay.");
 } else {
-    console.log("The price is too high.");
+    console.log("Peter's house: The price is too high.");
 }
 
 //Julia
@@ -47,7 +47,7 @@ const juliaHousePrice = 1000000;
 const juliaCalculatedHousePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 
 if (juliaHousePrice <= juliaCalculatedHousePrice) {
-    console.log("The price is okay.");
+    console.log("Julia's house: The price is okay.");
 } else {
-    console.log("The price is too high.");
+    console.log("Julia's house: The price is too high.");
 }
