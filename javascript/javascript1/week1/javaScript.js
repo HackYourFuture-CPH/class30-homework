a856dd37a74aa5a0cc4a3c689a8a051c2364e47b
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-11-12 20:33:28 +0100

    added boolean var

--- javascript/javascript1/week1/javaScript.js
+++ javascript/javascript1/week1/javaScript.js
@@ -5,3 +5,9 @@ const age= yearFuture - yearOfBirth;
 console.log("You will be " + age +" years old in " + yearFuture + ".");
 
 //Goodboy-Oldboy (A dog age calculator)
+const dogYearOfBirth = 2017;
+const dogYearFuture = 2027;
+const shouldShowResultInDogYears = true;
+const dogYearInHumanYear = dogYearFuture - dogYearOfBirth;
+const dogYearInDogYear = dogYearInHumanYear * 7
+
