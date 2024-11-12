3f664755788ca5828f57a579d2199b71d93c58c6
StefanyRomeroV <80517166+StefanyRomeroV@users.noreply.github.com>
2024-11-12 06:14:54 +0000

    update

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -1,6 +1,56 @@
 //Age calculator
 
-yearOfBirth = 1995
-yearFuture = 2027
-age = 2027-1995
-console.log("You will be " + age +" years old in"+ yearFuture)
+let yearOfBirth = 1995
+let yearFuture = 2027
+let age = yearFuture - yearOfBirth
+console.log("You will be " + age + " years old in" + yearFuture)
+
+//Goodboy-oldboy
+
+let dogYearOfbirth = 2018;
+let dogYearFuture = 2027;
+let dogYear = yearFuture - yearOfBirth;
+let shouldShowResultInDogYears = true;
+
+if (shouldShowResultInDogYears) {
+    let dogInDogYears = dogYear * 7;
+    console.log("Your dog will be ${dogInDogYears} dog years old in 2027");
+} else {
+    console.log("Your dog will be ${dogYear} human years old in 2027");
+}
+
+//Housey pricey
+
+const peterWidth = 8;
+const peterHigh = 10;
+const peterDeep = 10;
+const peterGarden = 100 ** 2;
+const peterHouse = 25000000;
+const volumeInMeters1 = peterWidth * peterHigh * peterDeep;
+const estimatePeterHouse = volumeInMeters1 * 2.5 * 1000 + peterGarden * 300;
+
+console.log("El precio estimado de la casa es " + housePricePeter);
+
+if (housePricePeter <= peterHouse) {
+    console.log("The pice is good")
+} else {
+    console.log("The price is too high")
+}
+
+const juliaWidth = 5;
+const julieaHigh = 11;
+const jualiaDeep = 8;
+const juliaHouse = 1000000;
+const juliaGarden = 70 ** 2;
+const volumeInMeters2 = jualiaDeep * juliaWidth * julieaHigh
+const hestimateJuliaHouse = volumeInMeters2 * 2.5 * 10000 + juliaGarden * 300;
+
+console.log("El precio estimado de la casa es " + housePriceJulia);
+
+if (housePriceJulia <= juliaHouse) {
+    console.log("The pice is good")
+} else {
+    console.log("The price is too high")
+}
+
+//
