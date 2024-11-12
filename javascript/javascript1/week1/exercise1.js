1092d098ed294ce0635cbaac24bdc0fc35e2e33d
andrpavl <puvlo@yahoo.com>
2024-11-12 20:25:35 +0100

    completed exercise1

---
+++ javascript/javascript1/week1/exercise1.js
@@ -0,0 +1,8 @@
+// Exercice 1 Age-ify (A future age calculator)
+
+const yearOfBirth = 1988;
+let yearFuture = 2047;
+
+const age = yearFuture - yearOfBirth;
+
+console.log("You will be " + age + " years old in " + yearFuture);
