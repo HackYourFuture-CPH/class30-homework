3517c4a703ba787121e1c132037feaac30bf1438
Ruslana Onyshchuk <lalelya7@gmail.com>
2024-11-13 15:51:55 +0100

    add task smart-ease

---
+++ javascript/javascript1/week1/script.js
@@ -0,0 +1,9 @@
+//FCC account: @ruslanaonshchk
+
+// Task: Smart-ease : Age-ify (A future age calculator)
+
+const yearOfBirth = 2001;
+const yearFuture = 2027;
+
+const age = yearFuture - yearOfBirth;
+console.log('You will be ' + age + ' years old in ' + yearFuture);
