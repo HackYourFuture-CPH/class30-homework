3c77eb7500c06d1323580b5c6d19265d38cac9b8
Ruslana Onyshchuk <lalelya7@gmail.com>
2024-11-13 16:00:18 +0100

    add task goodboy-oldboy(a dog age calculator

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -7,3 +7,26 @@ const yearFuture = 2027;
 
 const age = yearFuture - yearOfBirth;
 console.log('You will be ' + age + ' years old in ' + yearFuture);
+
+// Task : Goodboy-Oldboy (A dog age calculator)
+
+const dogYearOfBirth = 2019;
+let dogYearFuture = 2025;
+const dogYear = 7;
+const shouldShowResultInDogYears = true;
+
+// Calculate the dog's age in human years
+let dogAgeInHumanYears = dogYearFuture - dogYearOfBirth;
+
+// Calculate the dog's age in dog years
+let dogAgeInDogYears = dogAgeInHumanYears * dogYear;
+
+if (shouldShowResultInDogYears) {
+  console.log(
+    `Your dog will be ${dogAgeInDogYears} dog years old in ${dogYearFuture}`,
+  );
+} else {
+  console.log(
+    `Your dog will be ${dogAgeInHumanYears} human years old in ${dogYearFuture}`,
+  );
+}
