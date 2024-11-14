5d01100359523dda6906c27c288aa7bc79f962df
GuzideGuzelbey <guzide.guzelbey@gmail.com>
2024-11-14 16:48:07 +0100

    task 4 completed

---
+++ javascript/javascript1/week1/week1-guzide/ezNamey.js
@@ -0,0 +1,36 @@
+let firstWords = [
+  "Easy",
+  "Fantastic",
+  "Awesome",
+  "Start",
+  "Dummy",
+  "We-",
+  "Net",
+  "Quick",
+  "Pust-",
+  "Re-",
+];
+let secondWords = [
+  "Corp",
+  "Peasy",
+  "Dum",
+  "Off",
+  "Do-IT",
+  "X",
+  "IT",
+  "Go",
+  "Up",
+  "Hej!",
+];
+
+const randomNumber = Math.floor(Math.random() * 10);
+const startUpName = firstWords[randomNumber] + secondWords[randomNumber];
+const startUpNameLength = startUpName.length;
+
+console.log(
+  "The startup: " +
+    startUpName +
+    " contains " +
+    startUpNameLength +
+    " characters"
+);
