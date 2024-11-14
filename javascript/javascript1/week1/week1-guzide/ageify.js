429d008d9fd85d1ca07af249a9d9418b2b69481b
GuzideGuzelbey <guzide.guzelbey@gmail.com>
2024-11-14 17:00:58 +0100

    1st task simplified

--- javascript/javascript1/week1/week1-guzide/ageify.js
+++ javascript/javascript1/week1/week1-guzide/ageify.js
@@ -1,6 +1,6 @@
 const yearOfBirth = 1989;
 const yearFuture = 2034;
 
-const age = 2024 - yearOfBirth + (yearFuture - 2024);
+const age = yearFuture - yearOfBirth;
 
 console.log("You will be " + age + " years old in " + yearFuture);
