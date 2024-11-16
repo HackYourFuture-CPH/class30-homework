6cd4bd00f0c37c8672bb637a9027d4e1cb6617cb
GuzideGuzelbey <guzide.guzelbey@gmail.com>
2024-11-16 12:32:52 +0100

    peter part was forgotten and added and issue resolved

--- javascript/javascript1/week1/week1-guzide/houseyPricey.js
+++ javascript/javascript1/week1/week1-guzide/houseyPricey.js
@@ -12,11 +12,19 @@ const peterVolume = peterHouse.width * peterHouse.depth * peterHouse.height;
 const peterExpectedPrice =
   peterVolume * 2.5 * 1000 + peterHouse.gardenSizeInM2 * 300;
 
-if (peterHouse.currentPrice > peterExpectedPrice) {
+if (peterHouse.currentPrice < peterExpectedPrice) {
   console.log(
     "Hi " +
       peterHouse.customername +
       ", you are lucky! you are paying too little :) It is only " +
+      peterExpectedPrice +
+      "kr"
+  );
+} else if ((peterHouse.currentPrice = peterExpectedPrice)) {
+  console.log(
+    "Hi " +
+      peterHouse.customername +
+      " you calculated exactly what is offered to you: " +
       peterExpectedPrice +
       "kr"
   );
