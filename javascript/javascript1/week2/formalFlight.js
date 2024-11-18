9bf637ee8fc2b566d0fe3608a39dc6ec6a7b073d
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-11-18 20:27:03 +0100

    added "Madam"

--- javascript/javascript1/week2/formalFlight.js
+++ javascript/javascript1/week2/formalFlight.js
@@ -1,9 +1,13 @@
 let firstName= prompt("What is your first name?");
 let lastName= prompt("What is your last name?");
 let useFormalName =prompt("Do you whant to adress formal?")
+let gender= prompt("What is your gender?")
 function getFullname(firstName,lastName,useFormalName){
-    if (useFormalName= yes ){
-        return "Lord" + firstName + " "+ lastName;
+    if (useFormalName === "yes" || useFormalName === "Yes"  && gender==="male" || gender==="Male"){
+        return "Lord" + " " + firstName + " "+ lastName;
+    }
+    else if(useFormalName === "yes" || useFormalName === "Yes"  && gender==="female" || gender==="Female"){
+        return "Madam" + " " + firstName + " "+ lastName;
     }
     else{ 
         return firstName + " "+ lastName;}
