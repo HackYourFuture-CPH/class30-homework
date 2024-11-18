671c700891d90a285b7ffa7f503bc977296745c6
najmeh25 <seyedehnajmeh25@gmail.com>
2024-11-18 04:22:56 -0800

    chenge let  to const

--- javascript/javascript1/week1/age.js
+++ javascript/javascript1/week1/age.js
@@ -5,61 +5,66 @@ console.log("my age is"  + " " +  age + " " +  "for future");
 
 
 
-let dogYearOfBirth=2024;
-let dogYearFuture=2045;
+const dogYearOfBirth = 2015;
+const dogYearFuture = 2045;
+const shouldShowResultInDogYears = true;
 
- let shouldShowResultInDogYears=true;
-let dogAgeInHumanYears=dogYearFuture - dogYearOfBirth;
- let dogAgeInDogYears= dogAgeInHumanYears * 7;
+const dogAgeInHumanYears = dogYearFuture - dogYearOfBirth; 
+const dogAgeInDogYears = dogAgeInHumanYears * 7; 
 
-if(shouldShowResultInDogYears){
-    console.log(dogAgeInDogYears);
-}
-else{
-    console.log(dogAgeInHumanYears);
-}
-
-
-
-
-let volumeInMetersPeter = 8 * 10 * 10; 
-let gardenSizePeter = 100;
-let actualPricePeter = 2500000;
-
-
-let volumeInMetersJulia = 5 * 11 * 8;
-let gardenSizeJulia = 70;
-let actualPriceJulia = 1000000;
-
-
-let estimatedPricePeter = volumeInMetersPeter * 2.5 * 1000 + gardenSizePeter * 300;
-let estimatedPriceJulia = volumeInMetersJulia * 2.5 * 1000 + gardenSizeJulia * 300;
-
-
-if (actualPricePeter > estimatedPricePeter) {
-  console.log("Peter is paying too much for the house.");
+if (shouldShowResultInDogYears) {
+    console.log(`Your dog will be ${dogAgeInDogYears} dog years old in ${dogYearFuture}.`);
 } else {
-  console.log("Peter is paying a fair price for the house.");
-}
-
-if (actualPriceJulia > estimatedPriceJulia) {
-  console.log("Julia is paying too much for the house.");
-} 
-else {
-  console.log("Julia is paying a fair price for the house.");
+    console.log(`Your dog will be ${dogAgeInHumanYears} human years old in ${dogYearFuture}.`);
 }
 
 
 
-let firstWords = ["Easy", "Awesome", "Corporate", "Tech", "Creative", "Smart", "Innovative", "Future", "Next", "Quantum"];
-let secondWords = ["Solutions", "Systems", "Dynamics", "Design", "Labs", "Ventures", "Concepts", "Partners", "Works", "Global"];
+
+const Peter = {
+  width: 8,
+  depth: 10,
+  height: 10,
+  gardenSizeInM2: 100,
+  actualCost: 2500000,
+  estimatedPrice: 0
+};
+Peter.estimatedPrice = Peter.width * Peter.depth * Peter.height * 2.5 * 1000 + Peter.gardenSizeInM2 * 300;
+if (Peter.actualCost > Peter.estimatedPrice) {
+  console.log('Peter is paying too much. Estimated price: $${Peter.estimatedPrice}.');
+} else if (Peter.actualCost < Peter.estimatedPrice) {
+  console.log('Peter is paying too little. Estimated price: $${Peter.estimatedPrice}.');
+} else {
+  console.log('Peter is paying exactly the right amount. Estimated price: $${Peter.estimatedPrice}.');
+}
+const Julia = {
+  width: 5,
+  depth: 11,
+  height: 8,
+  gardenSizeInM2: 70,
+  actualCost: 1000000,
+  estimatedPrice: 0
+};
+Julia.estimatedPrice = Julia.width * Julia.depth * Julia.height * 2.5 * 1000 + Julia.gardenSizeInM2 * 300;
+if (Julia.actualCost > Julia.estimatedPrice) {
+  console.log('Julia is paying too much. Estimated price: $${Julia.estimatedPrice}.');
+} else if (Julia.actualCost < Julia.estimatedPrice) {
+  console.log('Julia is paying too little. Estimated price: $${Julia.estimatedPrice}.');
+} else {
+  console.log('Julia is paying exactly the right amount. Estimated price: $${Julia.estimatedPrice}.');
+}
+
+
+
+const firstWords = ["Easy", "Awesome", "Corporate", "Tech", "Creative", "Smart", "Innovative", "Future", "Next", "Quantum"];
+const secondWords = ["Solutions", "Systems", "Dynamics", "Design", "Labs", "Ventures", "Concepts", "Partners", "Works", "Global"];
 
 
 const randomFirstIndex = Math.floor(Math.random() * firstWords.length); 
 const randomSecondIndex = Math.floor(Math.random() * secondWords.length);
 
 
-let startupName = firstWords[randomFirstIndex] + " " + secondWords[randomSecondIndex];
+const startupName = firstWords[randomFirstIndex] + " " + secondWords[randomSecondIndex];
 
 
 console.log("The startup: \"" + startupName + "\" contains " + startupName.length + " characters.");
