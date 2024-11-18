2be9af2d8336297bf1a6a1975f1b21343f2f2d4a
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-11-18 20:01:47 +0100

    created function getFullname

---
+++ javascript/javascript1/week2/fligt.js
@@ -0,0 +1,7 @@
+let firstName= prompt("What is your first name?")
+let lastName= prompt("What is your last name?")
+function getFullname(firstName,lastName){
+    return firstName + " "+ lastName;
+}
+
+console.log(getFullname(firstName,lastName));
