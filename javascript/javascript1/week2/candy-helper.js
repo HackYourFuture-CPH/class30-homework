22a98fb775e30d49ecfe5e441bbb0b59c0b2148a
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2024-11-18 20:13:49 +0100

    use a while loop instead of a for loop.

--- javascript/javascript1/week2/candy-helper.js
+++ javascript/javascript1/week2/candy-helper.js
@@ -18,11 +18,19 @@ const amountToSpend = Math.random() * 100;
 
 function canBuyNewCandy(boughtCandyPrices){
   let total = 0;
+  let i = 0;
+  /*
   for (let i=0 ; i < boughtCandyPrices.length; i++){
     total += boughtCandyPrices[i];
   }
+  */
+  while ( i < boughtCandyPrices.length){
+    total += boughtCandyPrices[i];
+    i++;
+  }
   return total;
 }
+
 
 let total = canBuyNewCandy(boughtCandyPrices);
 if (total === 0) 
