f6373dd99dbe0512aeabc760d905349274a737f0
Ruslana Onyshchuk <lalelya7@gmail.com>
2024-11-18 17:11:56 +0100

    add task 1

---
+++ javascript/javascript1/week2/script.js
@@ -0,0 +1,11 @@
+// TASK Step 3: Smart-ease - Goes Global! : Flight booking fullname function
+
+function getFullname(firstname, surname) {
+  return firstname + ' ' + surname;
+}
+
+const fullName1 = getFullname('Ruslana', 'Onyshcuk');
+const fullName2 = getFullname('Andrii', 'Polishchuk');
+
+console.log(fullName1);
+console.log(fullName2);
