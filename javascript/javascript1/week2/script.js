158c6e1b65b19d8236ee60b00294c31890166260
Ruslana Onyshchuk <lalelya7@gmail.com>
2024-11-18 17:40:28 +0100

    add Formal fullname to task 1

--- javascript/javascript1/week2/script.js
+++ javascript/javascript1/week2/script.js
@@ -1,11 +1,36 @@
 // TASK Step 3: Smart-ease - Goes Global! : Flight booking fullname function
 
-function getFullname(firstname, surname) {
-  return firstname + ' ' + surname;
+function getFullname(
+  firstname,
+  surname,
+  useFormalName = false,
+  gender = 'male',
+) {
+  if (!firstname || !surname) {
+    return 'Invalid name';
+  }
+
+  if (useFormalName) {
+    if (gender === 'male') {
+      return 'Lord ' + firstname + ' ' + surname;
+    } else if (gender === 'female') {
+      return 'Lady ' + firstname + ' ' + surname;
+    } else {
+      return 'Dear ' + firstname + ' ' + surname;
+    }
+  } else {
+    return firstname + ' ' + surname;
+  }
 }
 
-const fullName1 = getFullname('Ruslana', 'Onyshcuk');
-const fullName2 = getFullname('Andrii', 'Polishchuk');
+let fullname1 = getFullname('Benjamin', 'Hughes');
+let fullname2 = getFullname('Anne', 'Hathaway', true, 'female');
+let fullname3 = getFullname('Cillian', 'Murphy', true, 'male');
+let fullname4 = getFullname('Emilia', 'Clarke', true, 'female');
+let fullname5 = getFullname('Taylor', 'Swift', true);
 
-console.log(fullName1);
-console.log(fullName2);
+console.log(fullname1);
+console.log(fullname2);
+console.log(fullname3);
+console.log(fullname4);
+console.log(fullname5);
