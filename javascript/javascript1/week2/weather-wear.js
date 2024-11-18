02ff36b3946a2811e51ee45e426a05f962d8bd08
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2024-11-18 19:55:20 +0100

    add weather wear

---
+++ javascript/javascript1/week2/weather-wear.js
@@ -0,0 +1,10 @@
+function dressForTheWeather (temperature){
+  return temperature > -10 && temperature <= 10 ? 'Wear a Jacket, Hat and Gloves' :
+         temperature > 10 && temperature <= 20 ? 'Wear Pants and Long Sleeves':
+         temperature > 20 && temperature <= 40 ? 'Wear Shorts and a T-Shirt':
+         'Stay Home! The weather is extreme!'
+}
+
+const clothesToWear = dressForTheWeather(18);
+
+console.log(clothesToWear);
