7353b685b56043bdf90ae6c834349406269a7b00
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-11-19 18:43:36 +0100

    added codes

--- javascript/javascript1/week2/student.js
+++ javascript/javascript1/week2/student.js
@@ -1,6 +1,36 @@
+let studentName= prompt("What is your name?")
+
 const class07Students = [];
 function addStudentToClass(studentName) {
-  // You write code here
+
+    // Check if the student is already in the class
+    if (class07Students.includes(studentName)) {
+        console.log(`Student ${studentName} is already in the class`);
+    }
+
+     // Check if the class is full
+    if (class07Students.length>7){
+      console.log ("Cannot add more students to class 07");
+      return;
+    }
+
+    // Special rule for the Queen
+    if (studentName === "Queen") {
+        class07Students.push(studentName);
+        console.log(`Student ${studentName} added to the class`);
+        return;
+    }
+
+    // Check if the student name is an empty string
+    if (studentName === "") {
+        console.log("Cannot add an empty name to the class");
+        return;
+    }
+
+       // Add the student to the class
+       class07Students.push[studentName];
+       console.log(`Student ${studentName} added to the class.`);
+
 }
 
 function getNumberOfStudents() {
