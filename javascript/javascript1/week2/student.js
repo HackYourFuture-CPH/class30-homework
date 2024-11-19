90545f49ef4ae0f94b66b08ffdc3b96567c86acb
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-11-19 19:12:15 +0100

    Final edit

--- javascript/javascript1/week2/student.js
+++ javascript/javascript1/week2/student.js
@@ -1,18 +1,18 @@
-let studentName= prompt("What is your name?")
+//addStudentToClass function
 
 const class07Students = [];
 function addStudentToClass(studentName) {
+
+    // Check if the student name is an empty string
+    if (!studentName || studentName.trim() === "") {
+        console.log("Cannot add an empty name to the class");
+        return;
+    }
 
     // Check if the student is already in the class
     if (class07Students.includes(studentName)) {
         console.log(`Student ${studentName} is already in the class`);
         return;
-    }
-
-     // Check if the class is full
-    if (class07Students.length>7){
-      console.log ("Cannot add more students to class 07");
-      return;
     }
 
     // Special rule for the Queen
@@ -22,17 +22,28 @@ function addStudentToClass(studentName) {
         return;
     }
 
-    // Check if the student name is an empty string
-    if (!studentName || studentName.trim() === "") {
-        console.log("Cannot add an empty name to the class");
-        return;
+     // Check if the class is full
+    if (class07Students.length>=6){
+      console.log ("Cannot add more students to class 07");
+      return;
     }
 
        // Add the student to the class
-       class07Students.push[studentName];
+       class07Students.push(studentName);
        console.log(`Student ${studentName} added to the class.`);
 
 }
 
-addStudentToClass(studentName);
+addStudentToClass("Parisa");
+addStudentToClass("Parisa");
+addStudentToClass("Milad");
+addStudentToClass("");
+addStudentToClass("Liam");
+addStudentToClass("Farzaneh");
+addStudentToClass("Milad");
+addStudentToClass("Nick");
+addStudentToClass("Jens");
+addStudentToClass("William");
+addStudentToClass("Ann");
+addStudentToClass("Queen");
 console.log(class07Students);
