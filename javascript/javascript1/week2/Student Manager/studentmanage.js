2f66f483ca0f5e6ffcffe3f0540683cd789857ef
gayathri <gayathri.cake@gmail.com>
2024-11-20 12:11:17 +0100

    student manager exercise completed

---
+++ javascript/javascript1/week2/Student Manager/studentmanage.js
@@ -0,0 +1,29 @@
+const class07Students = [];
+function addStudentToClass(studentName) {
+  if (studentName === "Queen" || studentName === "queen") {
+    class07Students.push(studentName);
+  } else if (studentName === "") {
+    console.log("Please enter your name");
+  } else if (class07Students.length >= 6) {
+    console.log("Cannot add more students to class07");
+  } else if (class07Students.includes(studentName)) {
+    console.log("Student " + studentName + " is already in the class");
+  } else class07Students.push(studentName);
+}
+
+function getNumberOfStudents() {
+  return class07Students.length;
+}
+
+addStudentToClass("Gayathri");
+addStudentToClass("Venkatraman");
+addStudentToClass("Ramadoss");
+addStudentToClass("Vaithiyanathan");
+addStudentToClass("");
+addStudentToClass("Deeksha");
+addStudentToClass("Gayathri");
+addStudentToClass("Queen");
+addStudentToClass("Lalitha");
+
+console.log(class07Students);
+console.log(getNumberOfStudents());
