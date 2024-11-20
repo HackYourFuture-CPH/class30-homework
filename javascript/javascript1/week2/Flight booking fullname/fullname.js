3ed2058c080024921016c59c349448fbf468c42e
gayathri <gayathri.cake@gmail.com>
2024-11-20 12:09:32 +0100

    Flight booking exercise completed

---
+++ javascript/javascript1/week2/Flight booking fullname/fullname.js
@@ -0,0 +1,19 @@
+let userFirstName = prompt("Enter your first name : ");
+let userSurName = prompt("Enter your Sur Name : ");
+let useFormalName = prompt("Do you want to use your formal name? (yes or no)");
+let userGender = prompt("Let me know your gender ? (male or female)");
+
+function getFullName(firstName, surName, formalName, gender) {
+  if (formalName.toLowerCase() === "yes" && gender.toLowerCase() === "male") {
+    return "Lord " + firstName + " " + surName;
+  } else if (
+    formalName.toLowerCase() === "yes" &&
+    gender.toLowerCase() === "female"
+  ) {
+    return "Mam " + firstName + " " + surName;
+  } else {
+    return firstName + " " + surName;
+  }
+}
+
+console.log(getFullName(userFirstName, userSurName, useFormalName, userGender));
