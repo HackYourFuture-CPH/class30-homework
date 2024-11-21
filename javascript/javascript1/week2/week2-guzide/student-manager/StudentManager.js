7f0cbd731b76a9199129e29f9896441d627e0883
GuzideGuzelbey <guzide.guzelbey@gmail.com>
2024-11-21 20:30:19 +0100

    Student Manager tool task is completed

---
+++ javascript/javascript1/week2/week2-guzide/student-manager/StudentManager.js
@@ -0,0 +1,37 @@
+const class07Students = [];
+
+function getNumberOfStudents() {
+  return class07Students.length;
+}
+
+function addStudentToClass(studentName) {
+  if (studentName == "") {
+    console.log("You can't leave this area empty");
+    return;
+  }
+
+  if (studentName === "Queen Mary") {
+    if (!class07Students.includes(studentName)) {
+      class07Students.push(studentName);
+      console.log(class07Students);
+    }
+    return;
+  }
+
+  if (getNumberOfStudents() >= 6) {
+    console.log("Cannot add more students to class 07");
+    return;
+  }
+  class07Students.push(studentName);
+  console.log(class07Students);
+}
+
+addStudentToClass("");
+addStudentToClass("Güzide");
+addStudentToClass("Mustafa");
+addStudentToClass("Jovita");
+addStudentToClass("Elitsa");
+addStudentToClass("Mads");
+addStudentToClass("Tuba");
+addStudentToClass("Oğuz");
+addStudentToClass("Queen Mary");
