e6fc350ce9a956884b9e004c3c9f47261e7b856a
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2024-11-21 15:20:57 +0100

    Add gender to function

--- javascript/javascript1/week2/fullname-function.js
+++ javascript/javascript1/week2/fullname-function.js
@@ -1,10 +1,17 @@
-function getFullname(firstname ,surname, useFormalName = false){
+function getFullname(firstname ,surname,  gender ='', useFormalName = true ){
   
   return firstname === '' || surname === '' ? 'Please provide both a first name and a surname.' :
-         useFormalName === true ?  'Lord '+ firstname+' '+ surname :  firstname+' '+ surname;
+         useFormalName === true && gender === 'male' ?  'Lord '+ firstname+' '+ surname :  
+         useFormalName === true && gender === 'female' ?  'lady '+ firstname+' '+ surname : firstname+' '+ surname;
 }
-const fullname1 = getFullname('Farzaneh', 'Ahmadi');
-const fullname2 = getFullname('Hamid', 'Khoshakhlagh', true);
+
+//test cases
+const fullname1 = getFullname('Farzaneh', 'Ahmadi', 'female');
+const fullname2 = getFullname('Hamid', 'Khoshakhlagh' , 'male', true);
+const fullname3 = getFullname('Parisa', 'Mosavi', 'female', true);
+const fullname4 = getFullname('Ali', 'Akbari', 'male',false);
 
 console.log(fullname1);
 console.log(fullname2);
+console.log(fullname3);
+console.log(fullname4);
