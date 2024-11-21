4e90aeb92475f24b8e010bf7bc8f1a919d0e6669
Ganja0003 <yuusuf98@outlook.com>
2024-11-21 17:35:19 +0100

    I forgot to add friday to  the days array, so i added it now. exercise 2 done.

--- javascript/javascript1/week2/event-app.js
+++ javascript/javascript1/week2/event-app.js
@@ -1,10 +1,10 @@
 function getEventWeekday(daysFromPresentDay){
 
-    const days = ["sunday","monday","tuesday","wednsday","thursday","saturday","sunday"];
+    const days = ["sunday","monday","tuesday","wednsday","thursday","friday","saturday","sunday"];
     const presentDay = new Date().getDay();
     const futureEventDay = (presentDay + daysFromPresentDay) % 7;
     return days[futureEventDay];
 }  
 
-
+console.log(getEventWeekday(1));
 
