11c70c4fbfc9f7d6fed44b7772c6ea6eaf4463b3
siderdk <idersoheib@gmail.com>
2024-11-22 09:43:27 +0100

    fix const where needed instead of let

--- javascript/javascript1/week1/homework_soheib/ezNamey.js
+++ javascript/javascript1/week1/homework_soheib/ezNamey.js
@@ -1,6 +1,6 @@
-let firstWords = ["One", "New", "Di", "All", "The", "Dan", "A", "Obsidian", "Cloud", "365"];
-let secondWords = ["Tech", "Jysk", "Dev", "Sol", "Suite", "In", "AI", "Click", "Soft", "Light"]
-let randomIndex1 = Math.floor(Math.random() * firstWords.length);
-let randomIndex2 = Math.floor(Math.random() * secondWords.length);
+const firstWords = ["One", "New", "Di", "All", "The", "Dan", "A", "Obsidian", "Cloud", "365"];
+const secondWords = ["Tech", "Jysk", "Dev", "Sol", "Suite", "In", "AI", "Click", "Soft", "Light"]
+const randomIndex1 = Math.floor(Math.random() * firstWords.length);
+const randomIndex2 = Math.floor(Math.random() * secondWords.length);
 let startupName = firstWords[randomIndex1]+secondWords[randomIndex2];
 console.log(`The startup: "${startupName}" contains ${startupName.length} characters.`)
