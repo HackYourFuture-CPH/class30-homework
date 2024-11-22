8010494ab993eb6e3b218acda5649107e8433d44
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-11-22 18:59:16 +0100

    Update javascript/javascript1/week2/flight.js
    Co-authored-by: Youssef Topaji <142032319+yousseftopaji@users.noreply.github.com>

--- javascript/javascript1/week2/flight.js
+++ javascript/javascript1/week2/flight.js
@@ -2,7 +2,7 @@
 let firstName= prompt("What is your first name?");
 let lastName= prompt("What is your last name?");
 function getFullname(firstName,lastName){
-    return firstName + " "+ lastName;
+    return `${firstName} ${lastName}`;
 }
 
 console.log(getFullname(firstName,lastName));
