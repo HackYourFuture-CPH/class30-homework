d885f41d94950b46bcdb88ad7e882d389db90b10
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-11-22 19:00:11 +0100

    Update javascript/javascript1/week2/event.js
    Added today
    
    Co-authored-by: Youssef Topaji <142032319+yousseftopaji@users.noreply.github.com>

--- javascript/javascript1/week2/event.js
+++ javascript/javascript1/week2/event.js
@@ -3,7 +3,7 @@ const daysFromToday= prompt("How many days from today is the event?");
 const today = prompt("Which day is today?")
 function getEventWeekday(daysFromToday,today){
     const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",]; 
-    const todayIndex = weekdays.indexOf();
+  const todayIndex = weekdays.indexOf(today);
     const eventDayIndex = (todayIndex + daysFromToday) % 7;
     return weekdays[eventDayIndex];
 }
