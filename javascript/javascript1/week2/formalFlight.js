ba2112152b76698ba190f0874ceb89908fc00268
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-11-22 18:54:35 +0100

    Update javascript/javascript1/week2/formalFlight.js
    revised the code
    
    Co-authored-by: Youssef Topaji <142032319+yousseftopaji@users.noreply.github.com>

--- javascript/javascript1/week2/formalFlight.js
+++ javascript/javascript1/week2/formalFlight.js
@@ -2,19 +2,22 @@
 let firstName= prompt("What is your first name?");
 let lastName= prompt("What is your last name?");
 let useFormalName =prompt("Do you whant to adress formal? (yes or no)")
-let gender= prompt("What is your gender? (male or female)")
+let gender;
 
-function getFullname(firstName,lastName,useFormalName,gender){
-    if (useFormalName.toLowerCase() === "yes" && gender.toLowerCase()==="male"){
-        return "Lord "  + firstName + " "+ lastName;
+function getFullname(firstName, lastName, useFormalName) {
+  if (useFormalName.toLowerCase() === "yes") {
+    gender = prompt("What is your gender? (male or female)");
+    if (gender.toLowerCase() === "male" || gender.toLowerCase() === "m") {
+      return "Mr. " + firstName + " " + lastName;
+    } else if (
+      gender.toLowerCase() === "female" ||
+      gender.toLowerCase() === "f"
+    ) {
+      return "Mrs. " + firstName + " " + lastName;
     }
-
-    else if(useFormalName.toLowerCase() === "yes" && gender.toLowerCase()==="female"){
-        return "Madam " + firstName + " "+ lastName;
-    }
-
-    else{ 
-        return firstName + " "+ lastName;}
+  } else {
+    return firstName + " " + lastName;
+  }
 }
 
-console.log(getFullname(firstName,lastName,useFormalName,gender));
+console.log(getFullname(firstName, lastName, useFormalName));
