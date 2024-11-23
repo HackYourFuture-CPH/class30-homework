2b72005639d923dbfadb114fca1cf0d2767e113a
pahan_an <andrey.handogy@gmail.com>
2024-11-23 11:46:39 +0100

    Task 4 changed

--- javascript/javascript1/week2/weather_wear.js
+++ javascript/javascript1/week2/weather_wear.js
@@ -1,18 +1,20 @@
 function whatWearToday(temperature){
-    if(temperature <= 5 && temperature > -20){
-        return("Put on warm pants, a warm jacket and a hat.");
-    }
     if(temperature<= -20){
-        return("Where are you??? Stay home!");
+        return "Where are you??? Stay home!";
     }
+
+    if(temperature > -20 && temperature <= 5 ){
+        return "Put on warm pants, a warm jacket and a hat.";
+    }
+    
     if(temperature > 5 && temperature <= 18){
-        return("Put on your pants and a sweatshirt.");
+        return "Put on your pants and a sweatshirt.";
     }
     if(temperature > 18 && temperature <= 35){
-        return("Put on shorts and a T-shirt.");
+        return "Put on shorts and a T-shirt.";
     }
     if(temperature > 35){
-        return("Put on your swimming trunks, ang go to sea immediately!");
+        return "Put on your swimming trunks, ang go to sea immediately!";
     }
 }
 
