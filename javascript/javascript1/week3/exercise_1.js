80d81c8a1ab1b55771fefb501b67d8de6375a0c5
andrpavl <puvlo@yahoo.com>
2024-11-25 21:09:24 +0100

    index.html added and exercise 1is done

---
+++ javascript/javascript1/week3/exercise_1.js
@@ -0,0 +1,18 @@
+const names = [
+	"Peter",
+	"Ahmad",
+	"Yana",
+	"kristina",
+	"Rasmus",
+	"Samuel",
+	"katrine",
+	"Tala",
+];
+const nameToRemove = "Ahmad";
+
+// Write some code here
+const indexOfNameToRemove = names.indexOf(nameToRemove);
+names.splice(indexOfNameToRemove, 1);
+// Code done
+
+console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
