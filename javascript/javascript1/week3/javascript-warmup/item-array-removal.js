70fee0fe600d2eedcf649ecbd21e04be992afb98
Ganja0003 <yuusuf98@outlook.com>
2024-11-25 15:20:57 +0100

    exercise 1 done

---
+++ javascript/javascript1/week3/javascript-warmup/item-array-removal.js
@@ -0,0 +1,22 @@
+const names = [
+    "Peter",
+    "Ahmad",
+    "Yana",
+    "kristina",
+    "Rasmus",
+    "Samuel",
+    "katrine",
+    "Tala",
+  ];
+  const nameToRemove = "Ahmad";
+  
+  // Write some code here
+
+  let indexOfNameToRemove = names.indexOf(nameToRemove)
+  
+  names.splice(indexOfNameToRemove,1)
+ 
+  // Code done
+  
+  console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+
