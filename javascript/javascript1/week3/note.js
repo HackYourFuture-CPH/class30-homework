4ed04e52baa152952db152c8d17515cbe269a26c
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-11-25 19:42:57 +0100

    changed the src

---
+++ javascript/javascript1/week3/note.js
@@ -0,0 +1,17 @@
+//NOnoN0nOYes (Note taking app)
+
+//Save a note
+
+const notes=[]
+let note={
+    content: "Pick up groceries", 
+    id:1
+}
+let note={
+    content:" Do laundry", 
+    id:1
+}
+function saveNote( content, id){
+   notes.push(note);
+}
+console.log(notes);
