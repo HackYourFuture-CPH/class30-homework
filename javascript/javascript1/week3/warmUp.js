252c78e49e10b093cb92cde90fa945b8a4641421
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-11-25 19:13:18 +0100

    created new function

--- javascript/javascript1/week3/warmUp.js
+++ javascript/javascript1/week3/warmUp.js
@@ -16,3 +16,18 @@ if (index !== -1) {
   names.splice(index, 1);
 }
   console.log(names);
+
+
+  const travelInformation = {
+    speed: 50,
+    destinationDistance: 432,
+  };
+  
+  function calculateTravelTime({speed, destinationDistance}){
+  const travelInHour= destinationDistance/speed;
+  const hours = Math.floor(timeInHours); // Extract whole hours
+  const minutes = Math.round((timeInHours - hours) * 60); // Convert fractional hours to minutes
+  return `${hours} hours and ${minutes} minutes`;
+  }
+  const travelTime= calculateTravelTime(travelInformation);
+  console.log(travelTime);
