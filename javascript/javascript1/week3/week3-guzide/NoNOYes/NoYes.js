a716b5f0d550bea7bcf02d25a3bbb20eaad5423e
GuzideGuzelbey <guzide.guzelbey@gmail.com>
2024-11-26 23:53:01 +0100

    bug in saving note function fixed and see note feature added

--- javascript/javascript1/week3/week3-guzide/NoNOYes/NoYes.js
+++ javascript/javascript1/week3/week3-guzide/NoNOYes/NoYes.js
@@ -1,7 +1,13 @@
 const notes = [];
 
 function saveNote(content, id) {
-  const newObject = { text: content, value: id };
+  if (typeof content !== "string") {
+    console.log("Error: 'content' should be a string");
+  }
+  if (typeof id !== "number") {
+    console.log("Error: 'id' should be a number");
+  }
+  const newObject = { content: content, id: id };
   notes.push(newObject);
 }
 
@@ -9,3 +15,18 @@ saveNote("Pick up groceries", 1);
 saveNote("Do laundry", 2);
 
 console.log(notes); // [{content: 'Pick up groceries', id: 1}, {content: 'Do laundry', id: 2}]
+
+function getNote(id) {
+  if (id === undefined || typeof id !== "number") {
+    console.log("Error: Invalid or Missing id");
+  }
+  for (let i = 0; i < notes.length; i++) {
+    if (id === notes[i].id) {
+      return notes[i];
+    }
+  }
+  console.log("Error: Note not found");
+}
+
+const firstNote = getNote(1);
+console.log(firstNote);
