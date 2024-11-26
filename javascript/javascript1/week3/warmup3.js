9e7e5e6285e27e99239d145f69a5cf91cafe53fc
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2024-11-26 12:41:15 +0100

    Add warmup 3

---
+++ javascript/javascript1/week3/warmup3.js
@@ -0,0 +1,39 @@
+const seriesDurations = [
+  {
+    title: "Game of thrones",
+    days: 3,
+    hours: 1,
+    minutes: 0,
+  },
+  {
+    title: "Sherlock",
+    days: 0,
+    hours: 18,
+    minutes: 0,
+  },
+  {
+    title: "Vikings",
+    days: 4,
+    hours: 13,
+    minutes: 0,
+  },
+];
+const myAge = 29;
+
+function calculateLifeSpentOnSeries(seriesArray, age){
+  const ageInHour = age * 365.25 * 24;
+  const result = [];
+
+  for (let i = 0; i<seriesArray.length; i++){
+    const seriesInHour = seriesArray[i].days * 24 + seriesArray[i].hours;
+    const percentage = (seriesInHour / ageInHour *100).toFixed(4);
+    result.push(`${seriesArray[i].title} took ${percentage}% of my life`)
+  }
+  return result
+}
+
+const lifeSpent = calculateLifeSpentOnSeries(seriesDurations, myAge);
+
+for (let i=0; i<lifeSpent.length; i++){
+  console.log(lifeSpent[i]);
+}
