f57366a2c046704337f583ac10aad49f7631b42d
Soheib <idersoheib@gmail.com>
2024-11-26 08:45:23 +0100

    clean branch from files from last week

--- javascript/javascript1/week1/homework_soheib/ezNamey.js
+++
@@ -1,6 +0,0 @@
-let firstWords = ["One", "New", "Di", "All", "The", "Dan", "A", "Obsidian", "Cloud", "365"];
-let secondWords = ["Tech", "Jysk", "Dev", "Sol", "Suite", "In", "AI", "Click", "Soft", "Light"]
-let randomIndex1 = Math.floor(Math.random() * firstWords.length);
-let randomIndex2 = Math.floor(Math.random() * secondWords.length);
-let startupName = firstWords[randomIndex1]+secondWords[randomIndex2];
-console.log(`The startup: "${startupName}" contains ${startupName.length} characters.`)
