f66fe13fd207a5cb5c4b05c5bb3ae8f43fabac24
Ruslana Onyshchuk <lalelya7@gmail.com>
2024-11-26 18:05:40 +0100

    add task about arraving time

--- javascript/javascript1/week3/script.js
+++ javascript/javascript1/week3/script.js
@@ -18,3 +18,20 @@ if (indexToRemove !== 1) {
 }
 
 console.log(names);
+
+//TASK: When will we be there??
+
+const travelInformation = {
+  speed: 50,
+  destinationDistance: 432,
+};
+
+function calculateTravelTime(info) {
+  const totalHours = info.destinationDistance / info.speed;
+  const hours = Math.floor(totalHours);
+  const minutes = Math.round((totalHours - hours) * 60);
+  return `${hours} hours and ${minutes} minutes`;
+}
+
+const travelTime = calculateTravelTime(travelInformation);
+console.log(travelTime);
