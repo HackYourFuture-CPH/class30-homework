10214aec7b14e02744fb5f2c6c0724f0f4cd9c42
Soheib <idersoheib@gmail.com>
2024-11-26 10:12:10 +0100

    finish warmUp.js

---
+++ javascript/javascript1/week3/homework5/warmUp.js
@@ -0,0 +1,24 @@
+const names = [
+    "Peter",
+    "Ahmad",
+    "Yana",
+    "kristina",
+    "Rasmus",
+    "Samuel",
+    "katrine",
+    "Tala",
+  ];
+  const nameToRemove = "Ahmad";
+  
+  // Write some code here
+
+  function removeNameFromArray(name, arr=names){
+    indexOfName = arr.indexOf(name);
+    return arr.splice(indexOfName, 1)
+  }
+
+  removeNameFromArray(nameToRemove, names);
+  
+  // Code done
+
+  console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
