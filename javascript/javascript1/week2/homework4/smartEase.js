1c560ac96718985ad982664baa465ad36028f1f5
Soheib <idersoheib@gmail.com>
2024-11-27 10:57:59 +0100

    remove nesting v1 in v2

--- javascript/javascript1/week2/homework4/smartEase.js
+++ javascript/javascript1/week2/homework4/smartEase.js
@@ -10,10 +10,10 @@ console.log(fullName2);
 
 function getFullnameV2(firstName, surname, useFormalName=true, gender ='other') {
     if( !useFormalName && gender === 'other'){
-        return getFullnameV1(firstName, surname)
+        return `${firstName} ${surname}`;
     }
     const prefix = gender === 'male' ? 'Mr.' : gender === 'female' ? 'Mrs.' : '';
-    return `${prefix}   ${getFullnameV1(firstName, surname)}`;
+    return `${prefix} ${firstName} ${surname}`;
   
 }
 console.log(getFullnameV2('Mark', 'Corrigan', true, 'male'));
