edd9e26730cdc46f9a183eb4795dc832014c2ff4
Soheib <idersoheib@gmail.com>
2024-11-27 10:30:42 +0100

    clean code by reordering the weekdays, removing the unnecessary if

--- javascript/javascript1/week2/homework4/eventApp.js
+++ javascript/javascript1/week2/homework4/eventApp.js
@@ -1,13 +1,7 @@
-const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-const today = new Date()
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 function getEventWeekDay(days) {
-    const day = today.getDay();
-    let result;
-    if(day + days <7){
-        result = day + days -1;
-    } else if (day + days >= 7) {
-        result = (day + days -1)%7
-    }
+    const today = new Date().getDay();;
+    const result = (days + today)%7
     return weekDays[result]
 }
 console.log(getEventWeekDay(5))
