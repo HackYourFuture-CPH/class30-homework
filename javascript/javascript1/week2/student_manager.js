6cc561966713ab33122bf549cd42a654cfa5fac9
jalilhu <jhus12b@gmail.com>
2024-12-02 15:17:01 +0100

    studentManager updated

--- javascript/javascript1/week2/student_manager.js
+++ javascript/javascript1/week2/student_manager.js
@@ -1,34 +1,43 @@
 const class07Students = [];
-function addStudentToClass(studentName = String) {
+function addStudentToClass(studentName) {
+  const criteriaToPass = {
+    isExist: class07Students.includes(studentName),
+    isLarger: class07Students.length >= 6,
+    isEmpty: studentName === "" || studentName === " ",
+    isQueen: studentName === "Queen",
+    isString: typeof studentName !== "string",
+  };
 
-let message = "Only strings are allowed."
-    if(typeof studentName !=='string'){
-        return message;
-    }else if(studentName !== " " && !class07Students.find(name => name === studentName && getNumberOfStudents.length < 6)){
-        class07Students.push(studentName);
-        message = `${studentName} has been added to class 07`;
-    }else if(studentName ==="Queen"){
-        class07Students.push(studentName);
-        message = `it is an honor to have the ${studentName} in class 07`;
-    }else if( class07Students.find(name => name === studentName)){
-        message = `Student ${studentName} is already in the class`;
-    }else if(studentName ==="" || studentName ===" "){
-        message = `Error: your student name is not acceptable.`;
-    }
-    else{
-        message = "please check your input again.";
-    }
-    return message;
+  if (criteriaToPass.isString) {
+    return "Only strings are allowed.";
+  }
+  if (criteriaToPass.isEmpty) {
+    return `Error: your student name is not acceptable.`;
+  }
+  if (criteriaToPass.isExist) {
+    return `Student ${studentName} is already in the class`;
+  }
+  if (criteriaToPass.isQueen) {
+    class07Students.push(studentName);
+    return `it is an honor to have the ${studentName} in class 07`;
+  }
+  if (criteriaToPass.isLarger) {
+    return `Student ${studentName} can not be added. no more space`;
   }
 
-
-function getNumberOfStudents() {
-    return class07Students.length;
+  class07Students.push(studentName);
+  return `${studentName} has been added to class 07`;
 }
 
-console.log(addStudentToClass("jalil"))
-console.log(addStudentToClass("jalil"))
-console.log(addStudentToClass(" "))
-console.log(addStudentToClass(5))
-
-console.log(class07Students)
+console.log(addStudentToClass("jalil"));
+console.log(addStudentToClass("Ahmad"));
+console.log(addStudentToClass("Reza"));
+console.log(addStudentToClass("MOhammad"));
+console.log(addStudentToClass("Panjs"));
+console.log(addStudentToClass("Shafiq"));
+console.log(addStudentToClass("Farhad"));
+console.log(addStudentToClass("Queen"));
+console.log(addStudentToClass(" "));
+console.log(addStudentToClass(""));
+console.log(addStudentToClass(5));
+console.log(class07Students);
