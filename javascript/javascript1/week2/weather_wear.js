71c360c71c37c3462bff69ae2163be74a1f578e7
jalilhu <jhus12b@gmail.com>
2024-12-02 15:32:35 +0100

    candy helper and weather wear

--- javascript/javascript1/week2/weather_wear.js
+++ javascript/javascript1/week2/weather_wear.js
@@ -1,15 +1,13 @@
-
-function suggestWhatToWear(num){
-    let youShoudWear = "nothing"
-    if( num >=18){
-        youShoudWear = "T-shirt and Shorts";
-    }else if( num >= 5 && num <= 18){
-        youShoudWear = "Sweater and Pants";
-    }else if(num < 5){
-        youShoudWear = "Jacket, Pants, Hat"
-    } else{
-        youShoudWear = "whatever you think keeps alive"
-    }
-    return youShoudWear;
+function suggestWhatToWear(num) {
+  if (num >= 18) {
+    return "T-shirt and Shorts";
+  }
+  if (num >= 5 && num <= 18) {
+    return "Sweater and Pants";
+  }
+  if (num < 5) {
+    return "Jacket, Pants, Hat";
+  }
+  return "whatever you think keeps alive";
 }
-console.log(suggestWhatToWear(-2))
+console.log(suggestWhatToWear(-2));
