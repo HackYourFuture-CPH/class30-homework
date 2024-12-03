b663c7a5eb2c583259a5ba4344045440d14fd770
Ganja0003 <yuusuf98@outlook.com>
2024-12-03 18:56:55 +0100

    I have modified the two first small functions inside the large function and the two first commands are working now.

--- javascript/javascript1/week4/voiceAssistant.js
+++ javascript/javascript1/week4/voiceAssistant.js
@@ -3,7 +3,7 @@ let theirName = undefined;
 function saveName(command){
     let name = command.split(" ")[4];
     theirName = name;
-    console.log(`Nice to meet you${theirName}`);
+    return(`Nice to meet you${theirName}`);
 }
 
 
@@ -14,10 +14,19 @@ function getName(){
     if(theirName){
         return (`You name is ${theirName}`);
     }else {
-        return ("Who are You ahahha");
+        return ("Who are You ahahhha");
     }
 }
 
 function getReply(command){
+        if(command.startsWith("Hi my name is")){
+            return saveName(command);
+        }
 
+
+        if (command == "What is my name?") {
+            return getName();
+        }
+
+        return "Im not advanced enough to answer this."
 }
