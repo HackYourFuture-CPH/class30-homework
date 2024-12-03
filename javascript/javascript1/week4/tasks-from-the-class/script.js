e9f6957b60443abab477815d17c81547eaa6ccf0
Ruslana Onyshchuk <lalelya7@gmail.com>
2024-12-03 19:41:27 +0100

    add fizzbuzz

--- javascript/javascript1/week4/tasks-from-the-class/script.js
+++ javascript/javascript1/week4/tasks-from-the-class/script.js
@@ -18,4 +18,19 @@ function fib(n) {
 console.log(fib(5));
 console.log(fib(10));
 
+//TASK FizzBuzz
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++){
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
 
+fizzBuzz();
