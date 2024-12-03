91c6d707a5ddbec0bf968211aa82478545fc8c7c
Ganja0003 <yuusuf98@outlook.com>
2024-12-03 17:44:40 +0100

    i have finishe d

--- javascript/javascript1/week4/voiceAssistant.js
+++ javascript/javascript1/week4/voiceAssistant.js
@@ -1,9 +1,9 @@
 let theirName = undefined;
 
 function saveName(command){
-let name = command.split(" ")[4];
-theirName = name;
-console.log(`Nice to meet you${theirName}`);
+    let name = command.split(" ")[4];
+    theirName = name;
+    console.log(`Nice to meet you${theirName}`);
 }
 
 
@@ -11,7 +11,11 @@ console.log(`Nice to meet you${theirName}`);
 
 
 function getName(){
-
+    if(theirName){
+        return (`You name is ${theirName}`);
+    }else {
+        return ("Who are You ahahha");
+    }
 }
 
 function getReply(command){
