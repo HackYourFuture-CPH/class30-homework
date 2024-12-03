ca5d1b7b3b187ce1d80af1ef4e5b239b8ca951ef
Ganja0003 <yuusuf98@outlook.com>
2024-12-03 17:31:18 +0100

    I have created a function that saves the name of the user and returns with a greeting

--- javascript/javascript1/week4/voiceAssistant.js
+++ javascript/javascript1/week4/voiceAssistant.js
@@ -1,6 +1,14 @@
-function saveName(){
+let theirName = undefined;
 
+function saveName(command){
+let name = command.split(" ")[4];
+theirName = name;
+console.log(`Nice to meet you${theirName}`);
 }
+
+
+
+
 
 function getName(){
 
