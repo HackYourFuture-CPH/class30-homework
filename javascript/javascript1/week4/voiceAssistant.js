5fd1f926eefbc7d3709ba3daf49f4ba04ba820ba
Ganja0003 <yuusuf98@outlook.com>
2024-12-05 20:31:09 +0100

    Added a function that adds a todo

--- javascript/javascript1/week4/voiceAssistant.js
+++ javascript/javascript1/week4/voiceAssistant.js
@@ -1,12 +1,13 @@
 let theirName = undefined;
+let todoList = [];
+
+
 
 function saveName(command){
     let name = command.split(" ")[4];
     theirName = name;
     return(`Nice to meet you${theirName}`);
 }
-
-
 
 
 
@@ -17,6 +18,20 @@ function getName(){
         return ("Who are You ahahhha");
     }
 }
+
+
+function addTodoList(command){
+    let todo = command.slice(4, command.indexOf("to my todo"));
+    todoList.push(todo);
+    return `${todo} added to you todo`;
+}
+
+
+
+
+
+
+
 
 function getReply(command){
         if(command.startsWith("Hi my name is")){
