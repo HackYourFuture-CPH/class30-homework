ec8fb497180bef258ce5c9070ef56d21e68c7bbd
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-12-09 12:10:36 +0100

    added card

---
+++ javascript/javascript2/week1/script.js
@@ -0,0 +1,6 @@
+// Select the container
+const cardContainer= document.getElementById('card-container');
+
+// Creat the card element
+const card=document.createElement('div');
+card.setAttribute('class', 'card');
