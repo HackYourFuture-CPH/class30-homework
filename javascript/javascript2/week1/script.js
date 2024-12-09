16332d13fa34583a4cc34f8f48e5c4b1831b14ba
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-12-09 12:54:33 +0100

    changed style

--- javascript/javascript2/week1/script.js
+++ javascript/javascript2/week1/script.js
@@ -14,8 +14,9 @@ card.innerHTML = `
   <img src="https://cdn.prod.website-files.com/5e4e48af45b75d848013007e/5e5948433f44fd4044f9a30b_kids_english_animals_turtle.png" alt="Placeholder Image">
   
 `;
-card.style.maxWidth='400px';
-card.style.aspectRatio = '4 / 3';
+card.style.Width='200px';
+card.style.height='300px';
+card.style.aspectRatio='0.75'
 card.style.borderRadius = '8px';
 card.style.boxShadow = '0 4px 8px';
 card.style.padding = '16px';
@@ -24,3 +25,6 @@ card.style.display = 'flex';
 card.style.justifyContent = 'center'; // Center items horizontally
 
 //Flipping
+card.addEventListener('click', function flipTheCard(){
+
+})
