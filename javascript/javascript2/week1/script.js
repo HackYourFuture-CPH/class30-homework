aa58c4552784d649add3459ee1c52b6209a5bfee
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-12-09 12:13:37 +0100

    appendchild

--- javascript/javascript2/week1/script.js
+++ javascript/javascript2/week1/script.js
@@ -4,3 +4,6 @@ const cardContainer= document.getElementById('card-container');
 // Creat the card element
 const card=document.createElement('div');
 card.setAttribute('class', 'card');
+
+//Append the card to the container
+cardContainer.appendChild(card);
