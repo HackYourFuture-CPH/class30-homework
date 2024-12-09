01a1ad465579727ed9250217624877cb1a028bca
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-12-09 12:38:52 +0100

    Styled the card

--- javascript/javascript2/week1/script.js
+++ javascript/javascript2/week1/script.js
@@ -7,3 +7,20 @@ card.setAttribute('class', 'card');
 
 //Append the card to the container
 cardContainer.appendChild(card);
+
+//Card style
+
+card.innerHTML = `
+  <img src="https://cdn.prod.website-files.com/5e4e48af45b75d848013007e/5e5948433f44fd4044f9a30b_kids_english_animals_turtle.png" alt="Placeholder Image">
+  
+`;
+card.style.maxWidth='400px';
+card.style.aspectRatio = '4 / 3';
+card.style.borderRadius = '8px';
+card.style.boxShadow = '0 4px 8px';
+card.style.padding = '16px';
+card.style.margin = '16px auto';
+card.style.display = 'flex';
+card.style.justifyContent = 'center'; // Center items horizontally
+
+//Flipping
