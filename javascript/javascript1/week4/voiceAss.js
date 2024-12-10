79f6fa2a90751f3e321ef9ccd380b09ec834dc08
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2024-12-10 11:50:05 +0100

    General changes

--- javascript/javascript1/week4/voiceAss.js
+++ javascript/javascript1/week4/voiceAss.js
@@ -1,94 +1,92 @@
+// Global variables for persistence
+let myName = '';
+let list = [];
 
 function getReply(command){
     // name
-    let sentence = 'Hello, my name is Benjamin.';
-    let parts = sentence.split(" ");
-    let name = parts.slice(4).join(" ");
+    if (command.startsWith("Hello, my name is")) {
+      let parts= command.split(" "); 
+     myName = parts.slice(4).join("");
+    return `Nice to meet you, ${myName}`;
+    }
+
+    if (command === "What is my name?") {
+        if(myName){
+        return `Your name is ${myName}`
+    } else{
+        return "I don't know your name yet."}
+    }
+
+    // Add do list
+    if (command.startsWith('Add') && command.includes('to my todo')) {
+        let parts2 = command.split(" ");
+        let todo = parts2.slice(1,-3).join(" ");
+        list.push(todo);
+        return `${todo} added to your todo.` ;
+    }
+    
+    //Remove from to do list
+    if (command.startsWith("Remove") && command.includes("from my todo")) {
+        let parts3= command.split(" ");
+        let todo= parts3.slice(1,-3).join(" ");
+        let index=list.indexOf(todo);
+        if (index > -1) {
+            list.splice(index, 1);
+            return `${todo} removed from your todo.`;
+        } else {
+            return `${todo} is not in your todo.`;
+        }
+    }
 
     // To do list
-    let list=[];
-    let toDoList='Add fishing to my todo';
-    let parts2 = toDoList.split(" ");
-    let toDo = parts2.slice(1,2).join(" ");
-    list.push(toDo);
+    if (command === "What is on my todo?") {
+    return `Your todo list: ${list.join(", ")}`;
+    }
 
-    // Making to do list
-    let addToDoList='Add singing in the shower to my todo';
-    let parts3 = addToDoList.split(" ");
-    let addToDo = parts3.slice(1,5).join(" ");
-    list.push(addToDo);
-    
     // Date
-    let today = new Date();
-    let humanReadable = today.toDateString();
+    if (command === "What day is it today?") {
+        let today = new Date();
+        let humanReadable = today.toDateString();
+        return  humanReadable;
+        
+    }
 
     // Math
-    let toDoMath = "What is 3+3";
-    let parts4 = toDoMath.split(" ");
+
+    if (command.startsWith("What is") && command.includes("+")){
+    let parts4 = command.split(" ");
     let expression= parts4.slice(2).join(" ");
     let result = eval(expression);
+        return result;
+    }
 
     // Timer
-    let setTimer='Set a timer for 4 minutes'
-    let parts5 = setTimer.split(" ");
+    if (command.startsWith("Set a timer for")) {
+        let parts5 = command.split(" ");
     let timer= parts5.slice(4).join(" ");
     let minutes = parseInt(timer);
     setTimeout(function() {
         console.log("Timer done");
     }, minutes * 60000);
+        return `Timer set for ${timer}.` ;
+    }
 
     // Time
-    let currentTime = new Date().toLocaleTimeString();
-
-    if (command === 'Hello, my name is Benjamin.') {
-        return `Nice to meet you, ${name}`;
-    } 
-    
-    else if (command === 'What is my name?') {
-        return `Your name is ${name}`;
+    if (command === 'What time is it?') {
+        let currentTime = new Date().toLocaleTimeString();
+        return `The current time is ${currentTime}.`;
     }
-    
-    else if (command === 'Add fishing to my todo') {
-    return `${toDo} added to your todo.` ;
+
+    return "I don't understand that command.";
 }
 
-else if (command === 'Add singing in the shower to my todo') {
-    return list ;
-}
-
-else if (command === 'Remove fishing from my todo') {
-    return `Removed ${toDo} from your todo.` ;
-}
-
-else if (command === 'What is on my todo?') {
-    return list ;
-}
-
-else if (command === 'What day is it today?') {
-    return humanReadable;
-}
-
-else if (command ==='What is 3+3'){
-    return result;
-}
-
-else if (command === 'Set a timer for 4 minutes') {
-    return `Timer set for ${timer}.` ;
-}
-
-else if (command === 'What time is it?') {
-    return `The current time is ${currentTime}.`;
-}
-
-}
-
-console.log(getReply('Hello, my name is Benjamin.'));
+console.log(getReply('Hello, my name is Parisa.'));
 console.log(getReply('What is my name?'));
-console.log(getReply('Add fishing to my todo'));
+console.log(getReply('Add diving to my todo'));
 console.log(getReply('Add singing in the shower to my todo'));
-console.log(getReply('Remove fishing from my todo'));
+console.log(getReply('Remove cyceling from my todo'));
 console.log(getReply('What is on my todo?'));
 console.log(getReply('What day is it today?'));
-console.log(getReply('What is 3+3'));
-console.log(getReply('Set a timer for 4 minutes'));
+console.log(getReply('What is 5+6'));
+console.log(getReply('Set a timer for 7 minutes'));
 console.log(getReply('What time is it?'));
