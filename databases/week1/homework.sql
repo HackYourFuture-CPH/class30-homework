84616fd70a33d3908de756f596e50b985327bbb2
najmeh25 <seyedehnajmeh25@gmail.com>
2025-02-07 09:30:38 -0800

    add change

--- databases/week1/homework.sql
+++ databases/week1/homework.sql
@@ -1,40 +1,51 @@
-SELECT * FROM HYF.task;
 
 -- 1. Find out how many tasks are in the task table
-SELECT count(*) AS total_taskS from hyf.task;
+SELECT count(id) AS total_taskS from task;
 
 -- 2. Find out how many tasks in the task table do not have a valid due date
-select * FROM hyf.task where due_date is NULL;
-select *from status;
+SELECT COUNT(id) FROM task
+WHERE due_date IS NULL;
+
 -- 3-Find all the tasks that are marked as done
-SELECT * FROM status WHERE name='done';
+SELECT title FROM task 
+WHERE status_id = (SELECT id FROM status
+WHERE name = 'Done');
 
 -- 4.Find all the tasks that are not marked as done
-select * FROM status where name!='done';
+SELECT title FROM task
+WHERE status_id != (SELECT id FROM status
+WHERE name = 'Done');
 
 -- 5. Get all the tasks, sorted with the most recently created first
-SELECT * FROM hyf.task ORDER BY created_at DESC;
+SELECT * FROM task
+ORDER BY created_at DESC;
 
 -- 6. Get the single most recently created task
-select * FROM hyf.task ORDER BY created_at DESC limit 1;
+SELECT * FROM task
+ORDER BY created_at DESC
+LIMIT 1;
+
 
 -- 7. Get the title and due date of all tasks where the title or description contains database
-SELECT title,status_id FROM hyf.task WHERE TITLE LIKE '%database%' OR description like '%database%';
+SELECT title, due_date FROM task
+WHERE title LIKE '%database%' OR description LIKE '%database%';
 
 -- 8. Get the title and status (as text) of all tasks
-SELECT hyf.task.title, hyf.status.name AS status
-FROM hyf.task
-JOIN hyf.status ON hyf.task.status_id = hyf.status.id;
+SELECT task.title AS Task, status.name AS Progress
+FROM task 
+INNER JOIN status ON task.status_id = status.id;
 
 -- 9. Get the name of each status, along with a count of how many tasks have that status
-SELECT hyf.status.name AS status, COUNT(task.id) AS task_count
-FROM hyf.task
-JOIN hyf.status ON hyf.task.status_id = hyf.status.id
-GROUP BY hyf.status.id
-ORDER BY task_count DESC;
+SELECT status.name as Status_name, COUNT(task.id) AS Status_name_count
+From status
+INNER JOIN task ON status.id = task.status_id
+GROUP BY status.name;
 
-SELECT hyf.status.name AS status, COUNT(task.id) AS task_count
-FROM hyf.status
-LEFT JOIN hyf.task ON status.id = task.status_id
-GROUP BY status.id
-ORDER BY task_count DESC;
+
+
+-- 10. Get the names of all statuses, sorted by the status with most tasks first
+SELECT status.name as Status_name, COUNT(task.id) AS Status_name_count
+FROM status
+INNER JOIN task ON status.id = task.status_id
+GROUP BY status.name
+ORDER BY Status_name_count DESC;
