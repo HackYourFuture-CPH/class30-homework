fe5c6c113cedcacff58239336b99550614f8ae8f
gayathri <gayathri.cake@gmail.com>
2025-02-07 10:37:43 +0100

    Updated queries based on revieew comments

--- databases/week1/homework.sql
+++ databases/week1/homework.sql
@@ -1,20 +1,20 @@
 -- Find out how many tasks are in the task table
-SELECT COUNT(*) as total_tasks FROM task
+SELECT COUNT(*) AS total_tasks FROM task
 
 -- Find out how many tasks in the task table do not have a valid due date
-SELECT * from task WHERE due_date is NULL
+SELECT count(*) AS totaltask_novalidduedate FROM task WHERE due_date IS NULL
 
 -- Find all the tasks that are marked as done
-SELECT * FROM task WHERE status_id IN (SELECT id from status where name LIKE "%DONE%")
+SELECT * FROM task WHERE status_id = (SELECT id FROM status WHERE name = 'done')
 
 -- Find all the tasks that are not marked as done
-SELECT * FROM task WHERE status_id not IN (SELECT id from status where name LIKE "%DONE%")
+SELECT * FROM task WHERE status_id NOT IN (SELECT id FROM status WHERE name LIKE "%DONE%")
 
 -- Get all the tasks, sorted with the most recently created first
-SELECT * from task ORDER BY created DESC
+SELECT * FROM task ORDER BY created DESC
 
 -- Get the single most recently created task
-SELECT * from task 
+SELECT * FROM task 
 ORDER BY created DESC
 LIMIT 1
 
@@ -29,14 +29,14 @@ FROM task
 INNER JOIN status ON task.status_id = status.id;
 
 -- Get the name of each status, along with a count of how many tasks have that status
-SELECT status.name as Status_Name, count(task.title) as Task_Count 
+SELECT status.name AS Status_Name, count(task.title) AS Task_Count 
 FROM task 
 INNER JOIN status 
 WHERE task.status_id = status.id 
 GROUP BY status.id
 
 -- Get the names of all statuses, sorted by the status with most tasks first
-SELECT status.name as Status_Name, count(task.title) as Task_Count 
+SELECT status.name AS Status_Name, count(task.title) AS Task_Count 
 FROM task 
 INNER JOIN status 
 WHERE task.status_id = status.id 
