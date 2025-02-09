4df6cadb45580df422496fffbfb3d0d983431084
Ruslana Onyshchuk <lalelya7@gmail.com>
2025-02-09 18:53:36 +0100

    correct mistakes in last 3rd tasks

--- databases/week1/homework.sql
+++ databases/week1/homework.sql
@@ -1,3 +1,7 @@
+select * from status;
+select * from user;
+select * from task;
+
 SELECT COUNT(*) FROM task;
 
 SELECT COUNT(*) FROM task WHERE due_date IS NULL;
@@ -16,3 +20,18 @@ SELECT * FROM task ORDER BY created DESC LIMIT 1;
 
 SELECT title, due_date FROM task 
 WHERE title LIKE '%database%' OR description LIKE '%database%';
+
+SELECT task.title, status.name AS status_text 
+FROM task
+JOIN status ON task.status_id = status.id;
+
+SELECT status.name, COUNT(task.id) AS task_count 
+FROM task
+JOIN status ON task.status_id = status.id
+GROUP BY status.name;
+
+SELECT status.name, COUNT(task.id) AS task_count 
+FROM task
+JOIN status ON task.status_id = status.id
+GROUP BY status.name
+ORDER BY task_count DESC;
