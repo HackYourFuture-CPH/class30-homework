30d917a18a2f131f99ef00cf008fa32a8201ac7c
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2025-02-11 19:29:55 +0100

    changed

--- databases/week2/database_tasks.sql
+++ databases/week2/database_tasks.sql
@@ -1,23 +1,26 @@
-USE DATABASE hyf_lesson2,
-insert into task (title, description, created, updated, due_date, status_id,, user_id) values ('Wash dishes', 'This is in priority.', '2025-02-11 06:54:16', '2025-02-11 06:54:16', '2025-02-12', 1, 123);
+USE hyf_lesson2;
 
-UPDATE tasks
-SET title = 'New Task Title'
+insert into task (title, description, created, updated, due_date, status_id) values ('Wash dishes', 'This is in priority.', '2025-02-11 06:54:16', '2025-02-11 06:54:16', '2025-02-12', 1);
+
+UPDATE task
+SET title = 'Go Playground'
 WHERE id = 5;
 
-UPDATE tasks
+UPDATE task
 SET due_date = '2025-02-25'
 WHERE id = 6;
 
-UPDATE tasks
+UPDATE task
 SET status_id = 2
 WHERE id = 7;
 
-UPDATE statuses
+UPDATE status
 SET name = 'Completed'
 WHERE id = 8;
 
-DELETE FROM tasks
+DELETE FROM task
 WHERE id = 9;
+
+SELECT* FROM task;
 
 
