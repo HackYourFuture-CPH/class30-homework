ab0858164a133012235ace63f72b25125a17ee53
gayathri <gayathri.cake@gmail.com>
2025-02-11 12:15:56 +0100

    completed homework

--- databases/week2/homework.sql
+++ databases/week2/homework.sql
@@ -1,3 +1,4 @@
+-- Active: 1738236443102@@0.0.0.0@3307@library_database
 SELECT * from task
 
 -----Add a task with these attributes: title, description, created, updated, due_date, status_id, user_id---
@@ -73,6 +74,44 @@ SELECT DATE_FORMAT(created, '%Y-%m') AS month, COUNT(*) AS task_count
 FROM task
 GROUP BY month
 ORDER BY month;
+
+-----CREATE TABLE statements------
+USE library_database; 
+CREATE TABLE Book (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    publication_year INT
+);
+
+CREATE TABLE Author (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE Member (
+    member_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE Borrowing_Log (
+    log_id INT PRIMARY KEY AUTO_INCREMENT,
+    book_id INT,
+    member_id INT,
+    borrow_date DATE NOT NULL,
+    return_date DATE,
+    FOREIGN KEY (book_id) REFERENCES Book(book_id),
+    FOREIGN KEY (member_id) REFERENCES Member(member_id)
+);
+
+CREATE TABLE Book_Author (
+    book_id INT,
+    author_id INT,
+    PRIMARY KEY (book_id, author_id),
+    FOREIGN KEY (book_id) REFERENCES Book(book_id),
+    FOREIGN KEY (author_id) REFERENCES Author(author_id)
+);
 
 
 
