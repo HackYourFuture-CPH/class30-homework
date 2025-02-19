3d3baedf7220936e85db61bf94169a2bb6510dee
GuzideGuzelbey <guzide.guzelbey@gmail.com>
2025-02-19 16:24:01 +0100

    updated the data structure according to the feedbacks given by the last session mentor

--- databases/week2/hw-part4.sql
+++ databases/week2/hw-part4.sql
@@ -3,55 +3,44 @@ USE mycompany_db;
 
 SET NAMES utf8mb4;
 
-CREATE TABLE `employee` (
+CREATE TABLE `emp` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` varchar(255) NOT NULL,
   `phone` varchar(255) NULL,
   `email` varchar(255) NOT NULL,
-  `jobtitle_id` int(10) unsigned NOT NULL,
-  `dept_id` int(10) unsigned NOT NULL,
-  CONSTRAINT `fk_jobtitle` FOREIGN KEY (`jobtitle_id`) REFERENCES `department_jobs` (`jobtitle_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_deptid` FOREIGN KEY (`dept_id`) REFERENCES `department_jobs` (`dept_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `jobt_id` int(10) unsigned NOT NULL,
+  `dep_id` int(10) unsigned NOT NULL,
+  CONSTRAINT `fk_j` FOREIGN KEY (`jobt_id`) REFERENCES `job` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_d` FOREIGN KEY (`dep_id`) REFERENCES `dep` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
-CREATE TABLE `job_titles` (
+
+
+CREATE TABLE `job` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `title` varchar(255) NOT NULL
 )
 
-CREATE TABLE `department` (
+CREATE TABLE `dep` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` varchar(255) NOT NULL
 )
 
-CREATE TABLE `department_jobs` (
-  `dept_id` int(10) unsigned NOT NULL,
-  `jobtitle_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`dept_id`, `jobtitle_id`),
-  CONSTRAINT `fk_deptid_deptjobs` FOREIGN KEY (`dept_id`) REFERENCES `department` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_jobtitleid_deptjobs` FOREIGN KEY (`jobtitle_id`) REFERENCES `job_titles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-)
 
 
-insert into employee (id, name, phone, email, jobtitle_id, dept_id) values (1, 'Jan Dark', '40404080', 'darkjan@company.dk', 1, 1);
-insert into employee (id, name, phone, email, jobtitle_id, dept_id) values (2, 'Ali Uyumaz', '50894910', 'uyurali@company.dk', 2, 2);
-insert into employee (id, name, phone, email, jobtitle_id, dept_id) values (3, 'Mette Hojbjergsen', '40989880', 'hojbmet@company.dk', 1, 1);
-insert into employee (id, name, phone, email, jobtitle_id, dept_id) values (4, 'Fariha Kerimi', '30304080', 'kerfarh@company.dk', 3, 1);
-insert into employee (id, name, phone, email, jobtitle_id, dept_id) values (5, 'Jovita Paulina', '29104070', 'paujov@company.dk', 4, 2);
+insert into emp (id, name, phone, email, jobt_id, dep_id) values (1, 'Jan Dark', '40404080', 'darkjan@company.dk', 1, 1);
+insert into emp (id, name, phone, email, jobt_id, dep_id) values (2, 'Ali Uyumaz', '50894910', 'uyurali@company.dk', 2, 2);
+insert into emp (id, name, phone, email, jobt_id, dep_id) values (3, 'Mette Hojbjergsen', '40989880', 'hojbmet@company.dk', 1, 1);
+insert into emp (id, name, phone, email, jobt_id, dep_id) values (4, 'Fariha Kerimi', '30304080', 'kerfarh@company.dk', 3, 1);
+insert into emp (id, name, phone, email, jobt_id, dep_id) values (5, 'Jovita Paulina', '29104070', 'paujov@company.dk', 4, 2);
 
-insert into job_titles (id, title) values (1, 'Software Engineer');
-insert into job_titles (id, title) values (2, 'Project Manager');
-insert into job_titles (id, title) values (3, 'UX_UI Designer');
-insert into job_titles (id, title) values (4, 'Business Analyst');
+insert into job (id, title) values (1, 'Software Engineer');
+insert into job (id, title) values (2, 'Project Manager');
+insert into job (id, title) values (3, 'UX_UI Designer');
+insert into job (id, title) values (4, 'Business Analyst');
 
 
 
-insert into department (id, name) values (1, 'Product Development');
-insert into department (id, name) values (2, 'Business Management');
+insert into dep (id, name) values (1, 'Product Development');
+insert into dep (id, name) values (2, 'Business Management');
 
-
-
-insert into department_jobs (jobtitle_id, dept_id) values (1, 1);
-insert into department_jobs (jobtitle_id, dept_id) values (3, 1);
-insert into department_jobs (jobtitle_id, dept_id) values (2, 2);
-insert into department_jobs (jobtitle_id, dept_id) values (4, 2);
