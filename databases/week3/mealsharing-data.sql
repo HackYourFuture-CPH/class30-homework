74138aa3f6366c5cc6859db03181c743fcd35c77
GuzideGuzelbey <guzide.guzelbey@gmail.com>
2025-02-21 18:54:57 +0100

    review table data inserted

--- databases/week3/mealsharing-data.sql
+++ databases/week3/mealsharing-data.sql
@@ -52,5 +52,17 @@ VALUES
 
 insert into Review (title, description, meal_id, stars, created_date) 
 VALUES
-(2, 2, '2024-11-10 20:30:25', '+36 3877848600', 'Borisova Minevka', 'bbmin@contact.com'),
-(3, 1, '2024-01-02 11:20:40', '+44 6677876557', 'Jack Mccormik', 'mcjackie@contact.com');
+('insanely delicious!', 'Our host was super welcoming and talented, <sarma> was amazing! Savory, sour, sweet and the taste was soo unique  mamma mia<3',  1, 5, '2025-02-28 20:30:25'),
+("A true delight!", "I had never tried Zeytinyağli Sarma before, but wow, what an explosion of flavors! Highly recommended.", 1, 4, "2025-02-15 18:34:21"),
+("Nice, but could be better", "The Zeytinyağli Sarma was decent, but I felt like it needed a bit more seasoning. It lacked some punch.", 1, 3, "2025-02-09 23:53:15"),
+("Not my cup of tea", "Maybe I had too high expectations, but Sarma didn't impress me. It was a bit bland for my liking.", 1, 1, "2025-01-31 21:45:40"),
+("Absolutely heavenly!", "Zeytinyağli Sarma was just perfect! Every bite was bursting with flavor, and the texture was spot on.", 1, 5, "2025-02-05 13:56:43"),
+("Super authentic!", "This tasted just like home-cooked food. Dalyan Köfte was prepared with so much love and care!", 2, 5, "2025-02-11 14:12:08"),
+("Okayish", "Dalyan Köfte was okay, but I found it a bit too oily. Not sure I-d order it again.", 2, 2, "2025-02-03 11:26:14"),
+("Good but not great", "Dalyan Köfte was tasty, but I found it a little too salty. Still worth trying!", 2, 3, "2025-02-18 17:29:55"),
+("Not bad, but expected more", "Dalyan Köfte was decent, but I was hoping for stronger flavors. It was a bit bland.", 2, 4, "2025-02-14 19:51:09"),
+("Loved it!", "This was my first time trying Dalyan Köfte, and it won't be my last. Amazing balance of spices!", 2, 5, "2025-02-07 20:45:32"),
+("Num num", "I had never tried Kuru-Pilav before, but wow, what an explosion of flavors! Highly recommended.", 3, 5, "2025-02-12 09:34:45"),
+("Not as promoted", "I am dissapointed a little, unfort Kuru-Pilav didn't impress me. A bit too dry for me.", 3, 2, "2025-02-01 07:52:11"),
+("Super authentic!", "This tasted just like real home-cooked food. Kuru-Pilav was a surprisingly delicious blend of two different taste", 3, 5, "2025-02-17 16:30:50"),
+("Could be better", "The Kuru-Pilav was decent, but I felt like it was too spicy. Burned till my ears.", 3, 3, "2025-02-06 22:15:29");
