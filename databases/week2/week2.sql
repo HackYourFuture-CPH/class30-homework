b1c2ceee8983a91f37842b5ef630d3559b62ab8c
StefanyRomeroV <80517166+StefanyRomeroV@users.noreply.github.com>
2025-03-03 08:35:47 +0100

    Update week2.sql

--- databases/week2/week2.sql
+++ databases/week2/week2.sql
@@ -1,27 +1,3 @@
-
-
-------------TASK PART 1------------
-
---Add a task with these attributes: title, description, created, updated, due_date, status_id, user_id
-SELECT * from task;
-insert into task (id, title, description, created, updated, due_date, status_id) value (38, 'Wash my clothes', 'Title says it all.', '2017-10-25 06:54:16', '2017-10-15 13:05:09', null, 2);
---Change the title of a task
-UPDATE task  SET title= 'My clothes' WHERE task.title ='Wash my clothes';
---Change a task due date// I changed all the nulls :( 
-UPDATE task SET due_date ='2017-12-07 23:04:38' WHERE due_date IS NULL;
---But now i Know that I should do this instead
-UPDATE task SET due_date= '2017-12-07 22:00:00' where id =38;
-
----Change a task status
-UPDATE task SET status_id = 1 WHERE id=38;
----Mark a task as complete--
-UPDATE task SET status_id=3 WHERE id=38;
-
---Delete a task
-DELETE from task WHERE id=37;
-
-
-
 ------------TASK PART 1------------
 
 --Add a task with these attributes: title, description, created, updated, due_date, status_id, user_id
@@ -101,7 +77,7 @@ SELECT task.title , task.created, `user`.name
 FROM task
 JOIN user_task ON task.id = user_task.task_id
 JOIN `user` on `user`.id = user_task.user_id
-WHERE `user`.name = 'Maryrose Meadows' AND MONTH(task.created) = 10;
+WHERE `user`.name = 'Maryrose Meadows' AND MONTH(task.created) = 09;
 
 -- Find how many tasks were created in each month
 SELECT YEAR(created) AS year, MONTHNAME(created) AS month, COUNT(*) AS task_count
