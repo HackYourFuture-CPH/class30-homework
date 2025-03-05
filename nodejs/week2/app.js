7ea10e5d0b92421a545e1236bcfefc2add2c080d
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2025-03-05 20:44:47 +0100

    Complete search route

--- nodejs/week2/app.js
+++ nodejs/week2/app.js
@@ -1,13 +1,33 @@
-import express from 'express'
+import express from 'express';
+import fs from 'fs';
 const app = express();
 const port = process.env.PORT || 3000;
 
+const documents = JSON.parse(fs.readFileSync('./documents.json', 'utf8'));
 // Support parsing JSON requests
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("This is a search engine");
 });
+//Get search
+app.get("/search",(req,res) =>{
+  const query = req.query.q;
+
+  if(!query){
+    return res.json(documents)
+  }
+
+  const filteredDocuments = documents.filter(doc =>
+    Object.values(doc).some(value =>
+      typeof value === 'string' && value.toLowerCase().includes(query.toLowerCase())
+    )
+  );
+
+  // Return filtered documents
+  res.json(filteredDocuments);
+});
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
