3f3b3b39da19da805f08504cd96a991e09d9dce2
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2025-03-05 22:31:00 +0100

    fix: Use process.exit(1) for error handling

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -4,11 +4,11 @@ const numbers = args.map(arg => parseFloat(arg));
 
 if(numbers.length === 0){
   console.log("Please enter some numbers!");
-  process.exit();
+  process.exit(1);
 }
 if(numbers.some(isNaN)){
   console.log("Error: All inputs must be valid numbers.")
-  process.exit();
+  process.exit(1);
 }
 let sum = 0;
 numbers.forEach(i => {
