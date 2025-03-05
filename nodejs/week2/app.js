4ee840b8d789e6067a8e56c427f7ff6f901cdac2
gayathri <gayathri.cake@gmail.com>
2025-03-05 11:18:23 +0100

    GET /search part completed

--- nodejs/week2/app.js
+++ nodejs/week2/app.js
@@ -1,12 +1,39 @@
 import express from "express";
+import fs from "fs";
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+const FILE_PATH_DOCUMENTS = "./documents.json";
+
+const readDocuments = () => {
+  try {
+    const data = fs.readFileSync(FILE_PATH_DOCUMENTS, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error reading documents JSON file:", error);
+    return [];
+  }
+};
 
 // Support parsing JSON requests
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("This is a search engine");
+});
+
+app.get("/search", (req, res) => {
+  const { q } = req.query;
+  let documents = readDocuments();
+  if (q) {
+    documents = documents.filter(
+      (document) =>
+        document.name.toLowerCase().includes(q.toLowerCase()) ||
+        document.description.toLowerCase().includes(q.toLowerCase())
+    );
+  }
+  res.status(200).json(documents);
 });
 
 app.listen(port, () => {
