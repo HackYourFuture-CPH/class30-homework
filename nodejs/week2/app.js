3f8e8cac891b446f45855e494966cd7da5be4368
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2025-03-05 20:58:14 +0100

    Complete GET /documents/:id

--- nodejs/week2/app.js
+++ nodejs/week2/app.js
@@ -1,5 +1,7 @@
+import { error } from 'console';
 import express from 'express';
 import fs from 'fs';
+import { parse } from 'path';
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -10,7 +12,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("This is a search engine");
 });
-//Get search
+//GET /search
 app.get("/search",(req,res) =>{
   const query = req.query.q;
 
@@ -23,10 +25,19 @@ app.get("/search",(req,res) =>{
       typeof value === 'string' && value.toLowerCase().includes(query.toLowerCase())
     )
   );
-
-  // Return filtered documents
   res.json(filteredDocuments);
 });
+// GET /documents/:id
+app.get("/documents/:id", (req,res)=>{
+  const documentID = parseInt(req.params.id,10);
+  const document = documents.find(doc => doc.id === documentID);
+  if(!document){
+    return res.status(404).json({error: "document not found"});
+  }
+
+  res.json(document);
+});
+
 
 
 app.listen(port, () => {
