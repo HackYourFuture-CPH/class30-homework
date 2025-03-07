c41fc7b56af14353ad396cd7a2a5a2ce1b44b5da
GuzideGuzelbey <guzide.guzelbey@gmail.com>
2025-03-07 15:44:59 +0100

    warmup task completed

--- nodejs/week2/app.js
+++ nodejs/week2/app.js
@@ -1,13 +1,15 @@
 import express from "express";
+import searchRouter from "./routes/search.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Support parsing JSON requests
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("This is a search engine");
 });
+
+app.use("/api", searchRouter);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
