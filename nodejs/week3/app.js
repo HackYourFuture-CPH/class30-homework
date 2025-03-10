fcf2794fd47a91fead975a0fa2b27f2827d1c334
gayathri <gayathri.cake@gmail.com>
2025-03-10 11:58:33 +0100

    warmup completed

--- nodejs/week3/app.js
+++ nodejs/week3/app.js
@@ -8,25 +8,29 @@ contactRouters.get("/", async (req, res) => {
   res.json({ message: "Contacts to display", result });
 });
 
-// contactsAPIRouter.get("/", async (req, res) => {
-//   let query = knexInstance.select("*").from("contacts");
+contactRouters.get("/", async (req, res) => {
+  let query = knex("contacts").select();
 
-//   if ("sort" in req.query) {
-//     const orderBy = req.query.sort.toString();
-//     if (orderBy.length > 0) {
-//       query = query.orderByRaw(orderBy);
-//     }
-//   }
+  const allowedSortColumns = ["id", "first_name", "last_name"];
 
-//   console.log("SQL", query.toSQL().sql);
+  const orderBy = req.query.sort ? req.query.sort.toString().trim() : "";
+  console.log("Received sort parameter:", orderBy);
 
-//   try {
-//     const data = await query;
-//     res.json({ data });
-//   } catch (e) {
-//     console.error(e);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// });
+  if (orderBy && allowedSortColumns.includes(orderBy)) {
+    query = query.orderBy(orderBy, orderBy === "last_name" ? "desc" : "asc");
+  } else {
+    return res.status(400).json({ message: "Invalid sort parameter" });
+  }
+
+  console.log("SQL", query.toSQL().sql);
+
+  try {
+    const data = await query;
+    res.json({ data });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 export default contactRouters;
--- nodejs/week3/database.js
+++ nodejs/week3/database.js
@@ -8,6 +8,7 @@ const connection = knex({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_NAME,
+    multipleStatements: true,
     ssl:
       process.env.DB_USE_SSL === "true" ? { rejectUnauthorized: false } : false,
   },
