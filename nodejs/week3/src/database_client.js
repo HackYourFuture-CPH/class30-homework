f9973e50f8c98c56f01eb273bee256e0dec0f5b2
StefanyRomeroV <80517166+StefanyRomeroV@users.noreply.github.com>
2025-04-07 08:30:10 +0200

    Update database_client.js

--- nodejs/week3/src/database_client.js
+++ nodejs/week3/src/database_client.js
@@ -1,8 +1,11 @@
 
 import "dotenv/config";
+console.log('DB_PORT', process.env.DB_PORT); // Debug check
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD); // Debug check
 console.log('DB_CLIENT:', process.env.DB_CLIENT); // Should log 'mysql2' or your desired client
 import knex from "knex";
+
+
 
 const database = knex({
   client: process.env.DB_CLIENT,
