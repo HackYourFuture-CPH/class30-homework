e45887bc0160ee2acdbf6e91fe40feb64c544100
gayathri <gayathri.cake@gmail.com>
2025-04-10 10:58:25 +0200

    NASA Mars Rover Photos

--- react/react2/week1/homework/next.config.mjs
+++ react/react2/week1/homework/next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "apod.nasa.gov",
       },
+      {
+        protocol: "http",
+        hostname: "mars.jpl.nasa.gov",
+      },
     ],
   },
 };
